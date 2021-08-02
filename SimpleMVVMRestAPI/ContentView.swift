53b0e272c805e1f6d232656f35dc8a567d8e95cf
davidyoon <qqpo12@naver.com>
2021-08-02 23:43:07 +0900

    add Course struct Model and courses [Course]

--- SimpleMVVMRestAPI/ContentView.swift
+++ SimpleMVVMRestAPI/ContentView.swift
@@ -7,9 +7,34 @@
 
 import SwiftUI
 
+struct Course:Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+    
+}
+
+
+
 class CoursesViewModel: ObservableObject {
     @Published var messages = "Message inside observable object"
+    
+    @Published var courses: [Course] = [
+        .init(name: "TEST_1"),
+        .init(name: "TEST_2"),
+        .init(name: "TEST_3")
+    ]
+    func changeMesssage() {
+        self.messages = "BLAH BLAH BLAH"
+    }
+    
+    
+    func fetchCourses() {
+        // fetch json and decode and update some array property
+    }
 }
+
+
+
 
 
 struct ContentView: View {
@@ -21,11 +46,17 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 Text(coursesVM.messages)
+                
+                ForEach(coursesVM.courses) { course in
+                    Text(course.name)
+                }
+                
+                
             }.navigationBarTitle("Courses")
             .navigationBarItems(trailing: Button(action: {
                 print("Fetching json data")
                 
-                self.coursesVM.messages = "SOMETHING ELSE"
+                self.coursesVM.changeMesssage()
             }, label: {
                 /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
             }))
